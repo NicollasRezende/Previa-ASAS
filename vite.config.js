3a8cc034c748bf6d5568fdf1e40f18a19c99e69f
NICOLLAS PEREIRA REZENDE <nicollaspereirarezende@outlook.com.br>
2025-01-06 13:34:24 -0300

    FixII

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: 'gzip',
-      ext: '.gz'
-    })
-  ],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          styles: ['styled-components']
-        }
-      }
-    }
-  }
-});
+  plugins: [react()],
+})
